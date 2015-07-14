6902b7ddfa020dc16a3bf127e460fe72e10ac04c
eenasm <eena.stanford@gmail.com>
2015-07-13 23:02:22 -0700

    First commit

---
+++ coding_challenge/src/median_unique.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import numpy
+
+try: 
+	# command line to specify input and output files
+	infile = sys.argv[1]
+	outfile = sys.argv[2]
+except IndexError:
+	# default input and output files
+	infile = "../tweet_input/tweets.txt"
+	outfile = "../tweet_output/ft2.txt"
+
+# read tweets.txt file
+
+# open input file for reading and output file for writing
+tweetfile = open(infile,"r")
+ft2 = open(outfile,"w")
+
+# initialize list for unique word count per tweet
+unique_word_cnt = []
+
+# read each tweet in tweet file, assuming one tweet per line 
+for line in tweetfile:
+	# initialize dictionary for word count
+	wordcount = {}
+
+	# count words in line 
+	for word in line.split():
+		if word not in wordcount:
+			wordcount[word] = 1
+		else:
+			wordcount[word] +=1
+	unique_word = len(wordcount)
+
+	# append unique word count to list of word counts per tweet
+	unique_word_cnt.append(unique_word)
+
+	# calculate running median and write to output file
+	median_unique = numpy.median(unique_word_cnt)
+	ft2.write(str(median_unique) + '\n')
+tweetfile.close
+ft2.close
---
+++ coding_challenge/src/words_tweeted.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+
+try:
+	# specify input and output files from command line 
+	infile = sys.argv[1]
+	outfile = sys.argv[2]
+except IndexError:
+	# default value for input and output files
+	infile = "../tweet_input/tweets.txt"
+	outfile = "../tweet_output/ft1.txt"
+
+# open input file for reading
+tweetfile = open(infile,"r")
+
+# initialize dictionary for word count
+wordcount = {}
+
+# read and count words per tweet, assuming one tweet per line
+for line in tweetfile:
+	for word in line.split():
+		if word not in wordcount:
+			wordcount[word] = 1
+		else:
+			wordcount[word] +=1
+tweetfile.close
+
+# sort words in dictionary
+sortedwords = sorted(wordcount)
+
+# write sorted words with corresponding word count to outfile
+ft1 = open(outfile,"w")
+for word in sortedwords:
+	ft1.write( "%s\t\t\t%d\n" % ( word, wordcount[word]) )
+ft1.close
